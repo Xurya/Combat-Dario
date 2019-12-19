2279e14c55b73da0c4e1d6ce9ad3b76188af3781
Xurya <xur1@bxscience.edu>
2019-12-19 15:49:57 -0500

    Updates

---
+++ Combat Dario/Assets/Scripts/BulletTime.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BulletTime : MonoBehaviour
+{
+    public static int DEFAULT_LAYER = 0;
+
+    [Header("Settings")]
+    public bool bulletTime = false;
+    public int layerToggle = 9;
+    public int duration = 0;
+    public int cooldown = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        bulletTime = !bulletTime;
+        if (bulletTime)
+        {
+            this.gameObject.layer = layerToggle;
+        }
+        else
+        {
+            this.gameObject.layer = DEFAULT_LAYER;
+        }
+    }
+}
